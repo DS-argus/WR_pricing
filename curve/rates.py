af8b71402dd4a997243bc6c917dda2f39ea222ed
argus <tjdals4047@gmail.com>
2022-06-01 18:59:38 +0900

    Add bs4 crawling

--- curve/rates.py
+++ curve/rates.py
@@ -1,13 +1,51 @@
 import pandas as pd
+from datetime import date
+
+from bs4 import BeautifulSoup
+import requests
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+"""
+금리 곡선을 도출하기 위한 데이터 수집
+단기(1년미만) : 콜금리 + CD 3개월
+장기(1년이상) : Swap rate
+"""
 
 
-class SeleniumCrawler:
-    chromedriver = r"C:\selenium\chromedriver.exe"
+def RatesCrawlerBS4(date: date) -> pd.DataFrame:
+    """ 주말이면 전영업일로 조회됨 """
+    url = 'https://www.bondweb.co.kr/Prime_web/menu03/sub08/kdb/swap_01LP2.asp?udate='
+
+    res = requests.get(url + date.strftime("%Y-%m-%d"))
+    data = BeautifulSoup(res.text, 'html.parser')
+
+    data = data.select('body>table')[1].select('tbody > tr')
+
+    maturity_list = []
+    rate_list = []
+    for dat in data:
+        info = dat.get_text().split()
+        maturity = f'{info[0]}Y'
+        mid_rate = (float(info[2]) + float(info[4])) / 2
+
+        maturity_list.append(maturity)
+        rate_list.append(mid_rate)
+
+    df = pd.DataFrame({'rate': rate_list}, index=maturity_list)
+
+    return df
+
+
+class RatesCrawlerSelenium:
+    chromedriver = r"C:\Users\tjdal\OneDrive\문서\.wdm\drivers\chromedriver\win32\102.0.5005.61\chromedriver.exe"
     IRS_url = "http://www.smbs.biz/Exchange/IRS.jsp"
     CD_url = 'http://www.smbs.biz/Bond/BondMajor.jsp'
     Treasury_url = 'http://www.smbs.biz/Bond/BondMajor.jsp'
+    IRSall_url = 'https://www.bondweb.co.kr/Prime_web/menu03/sub08/kdb/swap_01LP2.asp?udate=2022-05-30'
 
     def __init__(self):
         self.options = webdriver.ChromeOptions()
@@ -17,7 +55,7 @@ class SeleniumCrawler:
 
     def IRSrate(self):
 
-        driver = webdriver.Chrome(self.chromedriver, options=self.options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
         driver.get(self.IRS_url)
 
         data_maturity = driver.find_element(by=By.XPATH,
@@ -37,7 +75,7 @@ class SeleniumCrawler:
         return df
 
     def CDrate(self):
-        driver = webdriver.Chrome(self.chromedriver, options=self.options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
         driver.get(self.CD_url)
 
         data_CD91 = driver.find_elements(by=By.XPATH,
@@ -50,7 +88,7 @@ class SeleniumCrawler:
         return df
 
     def Treasuryrate(self):
-        driver = webdriver.Chrome(self.chromedriver, options=self.options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
         driver.get(self.Treasury_url)
 
         data_T_3Y = driver.find_elements(by=By.XPATH,
@@ -76,7 +114,10 @@ class SeleniumCrawler:
 
 
 if __name__ == "__main__":
-    irs = SeleniumCrawler()
-    print(irs.IRSrate())
-    print(irs.CDrate())
-    print(irs.Treasuryrate())
+    rates = RatesCrawlerSelenium()
+    print(rates.IRSrate())
+    # print(rates.CDrate())
+    # print(rates.Treasuryrate())
+
+    ref = date.today()
+    print(RatesCrawlerBS4(ref))
